5511798c16590b471dce3ed7408fc88829a848b7
s-fellner <74456354+s-fellner@users.noreply.github.com>
2021-04-11 18:02:24 +0200

    Update main.py

--- Application/main.py
+++ Application/main.py
@@ -20,8 +20,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def evaluation():
-        phrase = 'test'
+def root():
+        phrase = ''
     return render_template('index.html', phrase = phrase)
 
 if __name__ == '__main__':
