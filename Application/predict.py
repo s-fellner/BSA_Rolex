c35f1abadf4fa721994a02f449f016ebb3a6d285
s-fellner <74456354+s-fellner@users.noreply.github.com>
2021-04-11 19:40:25 +0200

    Update predict.py

--- Application/predict.py
+++ Application/predict.py
@@ -12,7 +12,7 @@ model_full_id = automl.AutoMlClient.model_path(project_id, "eu", model_id)
 # Supported mime_types: 'text/plain', 'text/html'
 # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#textsnippet
 
-def lvl_predict(content):
+def lvl(content):
     text_snippet = automl.TextSnippet(content=content, mime_type="text/plain")
     payload = automl.ExamplePayload(text_snippet=text_snippet)
     response = prediction_client.predict(name=model_full_id, payload=payload)
