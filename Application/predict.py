5f842ee66dee544c0d162ba0734e5b71dbab9221
s-fellner <74456354+s-fellner@users.noreply.github.com>
2021-04-12 11:40:33 +0200

    Update predict.py

--- Application/predict.py
+++ Application/predict.py
@@ -1,18 +1,21 @@
 from google.cloud import automl
 
-# TODO(developer): Uncomment and set the following variables
-project_id = "685330484131"
-model_id = "TCN4837384969284222976"
-# content = "text to predict"
+# You must first create a dataset, using the `eu` endpoint, before you can
+# call other operations such as: list, get, import, delete, etc.
+client_options = {'api_endpoint': 'eu-automl.googleapis.com:443'}
 
-prediction_client = automl.PredictionServiceClient()
+# TODO(developer): Uncomment and set the following variables
+project_id = '685330484131'
+model_id = 'TCN4837384969284222976'
+
+prediction_client = automl.PredictionServiceClient(client_options=client_options)
 
 # Get the full path of the model.
-model_full_id = automl.AutoMlClient.model_path(project_id, "us-central1", model_id)
+model_full_id = automl.AutoMlClient.model_path(project_id, "eu", model_id)
 
 # Supported mime_types: 'text/plain', 'text/html'
 # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#textsnippet
-def predict(content)
+def predict(content):
   text_snippet = automl.TextSnippet(content=content, mime_type="text/plain")
   payload = automl.ExamplePayload(text_snippet=text_snippet)
 
