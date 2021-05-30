6cace32a09fda4802b6e3a00597a04cab8659817
s-fellner <74456354+s-fellner@users.noreply.github.com>
2021-05-30 14:13:19 +0200

    Create test.py

---
+++ Code/Application/test.py
@@ -0,0 +1,9 @@
+from predict import full_pred, Monitor_Classification
+
+test = full_pred('Ceci est un test', monitor=True)
+test_moni = Monitor_Classification(test)
+
+top_score = ' with a probability of ' + str(round(test_moni['top_score'] * 100, 2)) + '%.'
+top_cat = 'level :'+ str(test_moni['top_cat'])
+
+print(top_cat, top_score)
